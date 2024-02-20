62ae005bd5f4e5378d51d5cf3b34b4cde22b39c7
mikairi <151601082+kyounaaa@users.noreply.github.com>
2024-02-20 13:51:28 +0700

    Add files via upload

---
+++ latihan_html1.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+    <tittle>ini situs miky</tittle>
+    </head>
+<body>
+    <table>
+    <table border ="1">
+        <td>
+        <tr>
+            <td style="background-color: yellow; ">No</td>
+            <td style="background-color:red;">Nama</td>
+            <td style="background-color:green;">Jenis Kelamin</td>
+            <td>Alamat</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Maszya Biandhara Ramadhani</td>
+            <td>Perempuan</td>
+            <td>Bogor</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Mikyona Ersa Janiandra</td>
+            <td>Perempuan</td>
+            <td>Jakarta</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Naya Aima Rustandi</td>
+            <td>Perempuan</td>
+            <td>Bogor</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Hasbi Maruf Kamil</td>
+            <td>Laki-laki</td>
+            <td>Bogor</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Putra Evan</td>
+            <td>Laki-Laki</td>
+            <td>Bogor</td>
+        </tr>
+<table border ="1">
+
+</table>
+   
+    
+</body>
+
+</html>
